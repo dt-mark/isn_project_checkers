cc4607a3af7ca906954daa443954660b2603ff07
dt-mark <best4tdm@gmail.com>
2018-01-13 15:37:19 +0100

    Update Checkers.py

--- Checkers.py
+++ Checkers.py
@@ -4,22 +4,24 @@ from colorsys import *
 
 #Tkinter
 window = Tk()
-window.title("Checkers")
+window.title("PROJET d'ISN")
 
 #Variables
+windowBorder = 50
+
 gSize = 10
-bSize = 4
+bSize = 40
 
 b = [[0 for i in range(gSize+1)] for j in range(gSize+1)]
 c = [[0 for i in range(gSize+1)] for j in range(gSize+1)]
 
 blackPlayers = [[0 for i in range(gSize+1)] for j in range(gSize+1)]
 whitePlayers = [[0 for i in range(gSize+1)] for j in range(gSize+1)]
-initialBlackIterator, initialWhiteIterator = 0, 0
+initialBlackCount, initialWhiteCount = 0, 0
 
 #Arrangement des frames principales
 mainFrame = Frame(window)
-mainFrame.pack(padx=50, pady=50)
+mainFrame.pack(padx=windowBorder, pady=windowBorder, fill="both")
 
 topFrame = Frame(mainFrame)
 topFrame.grid(row=0, column=0)
@@ -48,43 +50,65 @@ def case(i, j):
         return 0 
         
 #Fonction Principale 
-def click(button):
-    return button
+def click(event, i, j):
+    print("BUTTON PRESSED AT", i, ";", j)
     
 #Arrangement de la grille
 for i in range(gSize):
     for j in range(gSize):
-              
-        b[i][j] = Button(board, width=int(bSize), height=int(bSize/2), \
-                         command=lambda x=i, y=j : click(b[x][y]))
+        
+        #Création des boutons de base
+        b[i][j] = Canvas(board, width=bSize, height=bSize, bd=0, highlightthickness=0)
         b[i][j].grid(row=i, column=j)
         
+        #Création des jetons
+        #Si on est sur une case noire
         if case(i, j):
-            whitePlayers[i][j] = -1
-            c[i][j] = "#%02X%02X%02X" % (255, 255, 150)
-            if initialBlackIterator < gSize*2 and i >= 0:
+            #Créer la couleur marron
+            c[i][j] = "#%02X%02X%02X" % (150, 90, 0)
+            
+            #Si on a pas encore placé tous les jetons noirs (en haut de la grille)
+            if initialBlackCount < gSize*2 and i >= 0:
+                #Créer un cercle
                 blackPlayers[i][j] = Canvas(board, width=25, height=25, bg=c[i][j], bd=0, highlightthickness=0)
-                blackPlayers[i][j].create_oval(0, 0, 25, 25, fill="black", activewidth=0)
+                blackPlayers[i][j].create_oval(0, 0, 25, 25, fill="black", width=0)
                 blackPlayers[i][j].grid(row=i, column=j)
-                initialBlackIterator += 1
+                #Augmenter le compteur
+                initialBlackCount += 1
+            #Si on a placé tous les jetons noirs, le reste de la grille est égale à -1
             else:
                 blackPlayers[i][j] = -1
-        else:
-            blackPlayers[i][j] = -1
-            c[i][j] = "#%02X%02X%02X" % (50, 30, 0)
-            if initialWhiteIterator < gSize*2 and i >= 6:
+            
+            #Si on a pas encore placé tous les jetons blancs (en bas de la grille)
+            if initialWhiteCount < gSize*2 and i >= 6:
+                #Créer un cercle
                 whitePlayers[i][j] = Canvas(board, width=25, height=25, bg=c[i][j], bd=0, highlightthickness=0)
-                whitePlayers[i][j].create_oval(0, 0, 25, 25, fill="white", activewidth=0)
+                whitePlayers[i][j].create_oval(0, 0, 25, 25, fill="white", width=0)
                 whitePlayers[i][j].grid(row=i, column=j)
-                initialWhiteIterator += 1
-            else: 
+                #Augmenter le compteur
+                initialWhiteCount += 1
+            else:
                 whitePlayers[i][j] = -1
                 
-        b[i][j].config(relief=FLAT, state=NORMAL, background=c[i][j], activebackground=c[i][j])
+        #Si on est sur une case blanche
+        else:
+            #Créer la couleur crème
+            c[i][j] = "#%02X%02X%02X" % (255, 255, 150)
+            #Le reste de la grille est égal à -1
+            blackPlayers[i][j] = -1
+            whitePlayers[i][j] = -1
+       
+        #Fonction des boutons
+        b[i][j].bind("<1>", lambda event, x=i, y=j : click(event,x,y))
+        #Fonction des jetons (si jamais ils sont au dessus d'un bouton)
+        if blackPlayers[i][j] != -1: blackPlayers[i][j].bind("<1>", lambda event, x=i, y=j : click(event,x,y))
+        if whitePlayers[i][j] != -1: whitePlayers[i][j].bind("<1>", lambda event, x=i, y=j : click(event,x,y))
+        #Couleur des boutons   
+        b[i][j].config(background=c[i][j])
 
         
 #Arrangement du reste
-title = Label(topFrame, text="CHECKERS GAME", font=("Verdana", 25), height=1)
+title = Label(topFrame, text="LE JEU DE DAMES", font=("Verdana", 25), height=1)
 title.pack()
 sideCanvas = Canvas(sideFrame, width=500)
 sideCanvas.pack()
