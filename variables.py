d8939f07c3c7d04ee134c84dcfcb769e34333251
dt-mark <best4tdm@gmail.com>
2018-01-22 17:25:25 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,19 @@
+dico = {
+  "windowBorder":"The border between the window and everything else (mainFrame)"
+  "onePlayerCanEat":"Dictionnary which stores tuples of the coordinates of the players \
+                    that are able to eat another player for each player type"
+  "gSize":"Stands for grid size, defines the size of the grid (10x10 by default)"
+  "bSize":"Defines the size, in pixels, of one cell (40px by default)"
+  "playerCanvasSize":"Defines the size, in pixels, of one player \
+                     (smaller than the size of the cell)"
+}
 
+while True:
+  uInput = input(">>>")
+  try:
+    print(dico[uInput])
+  except:
+    print("Not in the database")
+  finally:
+    if uInput == "exit":
+      break
