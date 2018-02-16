6c33b1ccc17ab2e1fefface3bb8e7eb76fdbccb3
dt-mark <best4tdm@gmail.com>
2018-02-16 21:25:35 +0100

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,24 @@
+windowBorder = 50
+gSize = 10
+bSize = 40
+playerCanvasSize = 30
+
+colour = {
+    "red": "#%02X%02X%02X" % (200, 50, 50),
+    "green": "#%02X%02X%02X" % (0, 170, 50),
+    "white": "#%02X%02X%02X" % (255, 255, 130),
+    "black": "#%02X%02X%02X" % (160, 100, 0),
+    "blue": "#%02X%02X%02X" % (50, 125, 175),
+    "gold": "#%02X%02X%02X" % (255, 160, 60),
+    "purple": "#%02X%02X%02X" % (255, 130, 255),
+    "whitePlayer": "#%02X%02X%02X" % (255, 255, 255),
+    "blackPlayer": "#%02X%02X%02X" % (0, 0, 0)
+}
+sound = {
+    "click":"_menuClicked.wav",
+    "eat":"_playerAte.wav",
+    "move":"_playerMoved.wav",
+    "super":"_playerSuper.wav",
+    "select":"_playerSelected.wav",
+    "deselect":"_playerDeselected.wav"
+}
---
+++ globals.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+from constants import *
+
+b = [[0 for i in range(gSize + 1)] for j in range(gSize + 1)]
+c = [['' for i in range(gSize + 1)] for j in range(gSize + 1)]
+
+players, player = [[-1 for i in range(gSize + 1)] for j in range(gSize + 1)], -1
+onePlayerCanEat = {-1: [(-1, -1)], 1: [(-1, -1)]}
+scoreDisplay = {-1: StringVar(), 1: StringVar()}
+scoreDisplay[-1].set(str(gSize * 2))
+scoreDisplay[+1].set(str(gSize * 2))
+scorePlayer = {-1: [0 for i in range(gSize * 2 +1)], 1: [0 for i in range(gSize * 2 +1)]}
+turn = StringVar()
+turn.set("c'est au joueur {0} de jouer".format("BLANC" if player == -1 else "NOIR"))
+
+selectedPlayer = -1
+highlightStuck = False
+nothingHappened = 0
