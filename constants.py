7135f898269639c321c4a6a0d8f075b49bf39da3
Viciadev <35397356+Viciadev@users.noreply.github.com>
2018-03-09 19:24:36 +0100

    Add files via upload

--- constants.py
+++ constants.py
@@ -1,18 +1,37 @@
 windowBorder = 50
+globalWidth, globalHeight = 800, 500
+
 gSize = 10
 bSize = 40
 playerCanvasSize = 30
 
+refreshRate = 15
+
+mainFont = "Trebuchet MS"
 colour = {
-    "red": "#%02X%02X%02X" % (200, 50, 50),
-    "green": "#%02X%02X%02X" % (0, 170, 50),
     "white": "#%02X%02X%02X" % (255, 255, 130),
     "black": "#%02X%02X%02X" % (160, 100, 0),
+
+    "red": "#%02X%02X%02X" % (200, 50, 50),
+    "green": "#%02X%02X%02X" % (0, 170, 50),
     "blue": "#%02X%02X%02X" % (50, 125, 175),
     "gold": "#%02X%02X%02X" % (255, 160, 60),
     "purple": "#%02X%02X%02X" % (255, 130, 255),
     "whitePlayer": "#%02X%02X%02X" % (255, 255, 255),
-    "blackPlayer": "#%02X%02X%02X" % (0, 0, 0)
+    "blackPlayer": "#%02X%02X%02X" % (0, 0, 0),
+
+    "white0": "#%02X%02X%02X" % (255, 255, 130),
+    "black0": "#%02X%02X%02X" % (160, 100, 0),
+    "white1": "#%02X%02X%02X" % (255, 255, 200),
+    "black1": "#%02X%02X%02X" % (50, 75, 225),
+    "white2": "#%02X%02X%02X" % (255, 200, 100),
+    "black2": "#%02X%02X%02X" % (50, 75, 255),
+    "white3": "#%02X%02X%02X" % (255, 255, 205),
+    "black3": "#%02X%02X%02X" % (255, 100, 150),
+    "white4": "#%02X%02X%02X" % (180, 180, 180),
+    "black4": "#%02X%02X%02X" % (99, 23, 125),
+    "white5": "#%02X%02X%02X" % (180, 180, 180),
+    "black5": "#%02X%02X%02X" % (75, 75, 75)
 }
 sound = {
     "click":"_menuClicked.wav",
@@ -22,3 +41,6 @@ sound = {
     "select":"_playerSelected.wav",
     "deselect":"_playerDeselected.wav"
 }
+
+# "white4": "#%02X%02X%02X" % (255, 255, 130),
+# "black4": "#%02X%02X%02X" % (99, 23, 125),
