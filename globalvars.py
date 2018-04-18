6266c87176a3bc10b3a764ec2f16b7335f23b54c
dt-mark <best4tdm@gmail.com>
2018-04-18 15:16:47 +0200

    Update globalvars.py

--- globalvars.py
+++ globalvars.py
@@ -3,6 +3,10 @@ from tkinter import *
 from constants import *
 import optionvars
 
+class _StringVar(StringVar):
+    def __deepcopy__(self, memodict={}):
+        return self.get()
+
 window = Tk()
 
 b = [[0 for i in range(gSize + 1)] for j in range(gSize + 1)]
@@ -12,14 +16,17 @@ player = -1
 selectedPlayer = -1
 players = [[-1 for i in range(gSize + 1)] for j in range(gSize + 1)]
 
+eatenPlayers = [[-1 for i in range(gSize + 1)] for j in range(gSize + 1)]
+superPlayers = [[False for i in range(gSize + 1)] for j in range(gSize + 1)]
+
 onePlayerCanEat = {-1: [(-1, -1)], 1: [(-1, -1)]}
 onePlayerCanMove = {-1: [(-1, -1)], 1: [(-1, -1)]}
 
-scoreDisplay = {-1: StringVar(), 1: StringVar()}
+scoreDisplay = {-1: _StringVar(), 1: _StringVar()}
 scoreDisplay[-1].set(str(gSize * 2))
 scoreDisplay[+1].set(str(gSize * 2))
 
-scorePlayer = {-1: [0 for i in range(gSize * 2 +1)], 1: [0 for i in range(gSize * 2 +1)]}
+scorePlayer = {-1: [-1 for i in range(gSize * 2 +1)], 1: [-1 for i in range(gSize * 2 +1)]}
 
 turn = StringVar()
 turn.set("c'est au joueur {0} de jouer".format("BLANC" if player == -1 else "NOIR"))
@@ -44,3 +51,5 @@ globalTime.set(0)
 
 restart = IntVar()
 restart.set(0)
+undo = IntVar()
+undo.set(0)
